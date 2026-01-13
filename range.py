df4583c349f4847e12a9cd8587f65c1706b3a3b0
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-01-13 20:40:48 +0600

    Add files via upload

---
+++ range.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n + 1): # range(1, n+1) generates numbers from 1 to n
+        print(i, end="") #end="" prevents a new line after each number, so they print together
