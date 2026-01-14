556f87e4ea1b48886f57b61823b0cc1fcac1f999
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-01-14 19:49:42 +0600

    Add files via upload

--- runner up.py
+++ runner up.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
     n = int(input())
-    a = list(map(int, input().split())) 
+
     #This .splits() use for split the string by spaces and makes a list of strings
     #map() applies a function to every item in the list
+    a = list(map(int, input().split())) 
+   
     
     # Remove duplicate scores
     b = list(set(a))
