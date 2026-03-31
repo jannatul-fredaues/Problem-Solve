35506eb77970d1694532e320e9ad48166a98d6d6
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-03-31 23:05:50 +0600

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,13 @@
+a ="Hello World"
+
+print(a) #Hello World
+print(type(a)) #<class 'str'>
+print(a[0]) # H
+print(a[-1]) # d
+print(a[0:5]) # Hello
+print(a[6:11]) # World
+print(a[:5]) # Hello
+print(a[::2]) # HloWrd
+print(a.upper()) # HELLO WORLD
+print(a.lower()) # hello world
+print(a.replace("World", "Python")) # Hello Python
