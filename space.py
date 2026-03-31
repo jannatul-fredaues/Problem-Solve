7921ba588e94ab5b907ba45440844e3f71ad17e7
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-03-31 23:06:34 +0600

    Add files via upload

---
+++ space.py
@@ -0,0 +1,6 @@
+#combine strings with a separator and end character
+
+print("My", "name", "is", "Soha ", sep="-", end="!")
+
+#print a string with a space separator by default
+print("I", "am", "a", "student")
