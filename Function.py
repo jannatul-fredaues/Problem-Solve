6666d0e6b1daf0bd86a131f21a53ed8e670bd885
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-04-10 18:43:48 +0600

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5]
+x=len(my_list)
+print(x) # This will print the length of the list, which is 5
+y=sum(my_list)
+print(y) # This will print the sum of the elements in the list, which is 15
+z = max(my_list)
+print(z) # This will print the maximum value in the list, which is 5
