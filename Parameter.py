54e3e7a3e46803c3c6547849b45318582fb532bf
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-04-10 18:45:00 +0600

    Add files via upload

---
+++ Parameter.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print("hi " + fname)
+my_function("Soha") # This will print "hi Soha"
+my_function("Rahman") # This will print "hi Rahman"   
+my_function("Shawon") # This will print "hi Shawon"
+    
