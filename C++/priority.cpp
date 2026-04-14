361ffbe31c799d1d1a1c10ab6668d8c546514151
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-04-14 19:26:56 +0600

    Add files via upload

---
+++ C++/priority.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    int at[n], bt[n], pr[n];
+    int ct[n], tat[n], wt[n];
+    bool done[n] = {false};
+
+    cout << "Enter Arrival Time:\n";
+    for (int i = 0; i < n; i++)
+        cin >> at[i];
+
+    cout << "Enter Burst Time:\n";
+    for (int i = 0; i < n; i++)
+        cin >> bt[i];
+
+    cout << "Enter Priority (lower number = higher priority):\n";
+    for (int i = 0; i < n; i++)
+        cin >> pr[i];
+
+    int completed = 0, time = 0;
+
+    while (completed < n) {
+        int idx = -1, highest = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && !done[i] && pr[i] < highest) {
+                highest = pr[i];
+                idx = i;
+            }
+        }
+
+        if (idx == -1) {
+            time++;
+        } else {
+            time += bt[idx];
+            ct[idx] = time;
+            tat[idx] = ct[idx] - at[idx];
+            wt[idx] = tat[idx] - bt[idx];
+
+            done[idx] = true;
+            completed++;
+        }
+    }
+
+    float avgWT = 0, avgTAT = 0;
+
+    cout << "\nProcess\tAT\tBT\tPR\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; i++) {
+        cout << "P" << i+1 << "\t" << at[i] << "\t" << bt[i]
+             << "\t" << pr[i] << "\t" << ct[i]
+             << "\t" << tat[i] << "\t" << wt[i] << endl;
+
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
+    cout << "\nAverage Waiting Time = " << avgWT / n;
+    cout << "\nAverage Turnaround Time = " << avgTAT / n;
+
+    return 0;
+}
