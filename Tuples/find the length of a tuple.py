91618a6a4fcd5e9428f2dd4ce5033b005cae6f8e
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-04-18 10:05:07 +0600

    Add files via upload

---
+++ Tuples/find the length of a tuple.py
@@ -0,0 +1,8 @@
+#create a tuple
+tuplex = tuple("w3resource")
+print(tuplex)
+#use the len() function to known the length of tuple
+print(len(tuplex))
+
+#output: ('w', '3', 'r', 'e', 's', 'o', 'u', 'r', 'c', 'e')
+#10
