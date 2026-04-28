ec38a889549bba588256dceab1f00c55110e2ab7
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-04-28 09:38:19 +0600

    Delete basics  directory

--- basics /dictionare.py
+++
@@ -1,9 +0,0 @@
-thisdist={
-    "name":"dictionare",
-    "version":"0.1", 
-    "author":"John Doe",
-    "description":"This is a sample dictionare for demonstration purposes.",
-    "keywords":["dictionare", "python", "example"],
-    "license":"MIT",
-    "year":2024
-}
