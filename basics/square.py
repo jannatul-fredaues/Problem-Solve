289ba28add1e67c5c0d7b7cb2048f6d3a4a24bad
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-05-01 09:00:51 +0600

    Rename square.py to basics/square.py

---
+++ basics/square.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i * i)// print function must have 1 space or 4 spaces indentation
--- square.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        print(i * i)// print function must have 1 space or 4 spaces indentation
